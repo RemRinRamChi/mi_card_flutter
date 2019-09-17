b5e31cf7aaaf353ec4799fa08cef80424ca9b8ef
Yao Jian Yap <yapyaojian731@gmail.com>
2019-09-17 21:39:12 +0800

    Completed layout practice challenge

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,47 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                width: 100,
+                color: Colors.red[700],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.yellow[700],
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.green[700],
+                  ),
+                ],
+              ),
+              Container(
+                width: 100,
+                color: Colors.blue[700],
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }

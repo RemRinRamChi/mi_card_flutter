94dc34e9a3e1077690f96af2f969b1d15df1133f
Yao Jian Yap <yapyaojian731@gmail.com>
2019-09-17 22:42:34 +0800

    Added rounded image, name label, occupation label and fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/leorio.jpg'),
+              ),
+              Text(
+                'Leorio Paladiknight',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40,
+                  fontFamily: 'Righteous',
+                ),
+              ),
+              Text(
+                'Hunter | Medical Student'.toUpperCase(),
+                style: TextStyle(
+                  color: Colors.teal[100],
+                  fontSize: 20,
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2,
+                ),
+              )
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
